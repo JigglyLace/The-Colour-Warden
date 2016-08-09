c7f9a546290de6cbaa7f6a3359b462eb92409d35
Neil Coleman <yourpalneil@gmail.com>
2016-08-09 05:43:15 +0100

    09-08-2016 05:45

---
+++ The Colour Warden/Assets/Standard Assets/Scripts/ColourPicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColourPicker : MonoBehaviour
+{
+    private new Renderer renderer = new Renderer();
+    void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    public void SetRendererColour(Color32 i_Colour)
+    {
+        renderer.material.SetColor("_Color", i_Colour);
+    }
+
+    public Color32 GetRendererColour()
+    {
+        return renderer.material.GetColor("_Color");
+    }
+}
